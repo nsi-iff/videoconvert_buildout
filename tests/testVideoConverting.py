095fc874d900a0b4aebbb14036a36721ad614017
Douglas Camata <d.camata@gmail.com>
2011-03-26 01:36:39 +0000

    testes totalmente adaptados à nova arquitetura.

--- tests/testVideoConverting.py
+++ tests/testVideoConverting.py
@@ -16,8 +16,8 @@ FOLDER_PATH = abspath(dirname(__file__))
 class VideoConvertTest(unittest.TestCase):
 
     def setUp(self):
-        self.video_service = Restfulie.at("http://test:test@localhost:8080/").as_('application/json').accepts('application/json')
-        self.sam = Restfulie.at('http://localhost:8888/').as_('application/json')
+        self.video_service = Restfulie.at("http://localhost:8080/").auth('test', 'test').as_('application/json')
+        self.sam = Restfulie.at('http://localhost:8888/').auth('test', 'test').as_('application/json')
         self.uid_list = []
 
     def testConvertion(self):
@@ -33,30 +33,32 @@ class VideoConvertTest(unittest.TestCase):
 
         self.assertTrue(self.video_service.get({'key':uid}).resource().done)
         video = loads(self.sam.get({'key':uid}).body)
+
         self.assertTrue(isinstance(video, dict))
-        self.assertEquals(len(video), 3)
+        self.assertEquals(len(video), 4)
 
         video_data = decodestring(video.get('data'))
         self.assertTrue(video_data)
 
 if __name__ == '__main__':
+        rabbitmq_ctl = join(FOLDER_PATH, '..', 'bin', 'rabbitmqctl')
+        rabbitmq_server = join(FOLDER_PATH, '..', 'bin', 'rabbitmq-server')
         videoconvert_ctl = join(FOLDER_PATH, '..', 'bin', 'videoconvert_ctl')
-        restmq_ctl = join(FOLDER_PATH, '..', 'bin', 'restmq_ctl')
-        slave = join(FOLDER_PATH, '..', 'etc', 'slave.py')
+        worker = join(FOLDER_PATH, '..', 'bin', 'start_worker -name test_worker')
+        stop_worker = join(FOLDER_PATH, '..', 'bin', 'stop_worker')
         add_user = join(FOLDER_PATH, '..', 'bin', 'add-user.py')
         del_user = join(FOLDER_PATH, '..', 'bin', 'del-user.py')
         try:
-            call("%s start" % restmq_ctl, shell=True)
+            call("%s -detached" % rabbitmq_server, shell=True)
             call("%s start" % videoconvert_ctl, shell=True)
             call("%s test test" % add_user, shell=True)
-            slave_process = Process(target=call, args=(slave,), kwargs={"shell":True})
-            slave_process.start()
+            call("%s" % worker, shell=True)
             sleep(5)
             unittest.main()
         finally:
-            slave_process.terminate()
             sleep(1)
-            call("%s stop" % restmq_ctl, shell=True)
+            call("%s stop" % rabbitmq_ctl, shell=True)
             call("%s stop" % videoconvert_ctl, shell=True)
+            call("%s test_worker " % stop_worker, shell=True) 
             call("%s test" % del_user, shell=True)
 
